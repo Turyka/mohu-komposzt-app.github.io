2577487c1e41c3d55022aabee45ae7b854da84cd
Turi Gergo <87238018+Turyka@users.noreply.github.com>
2026-03-26 13:05:56 +0100

    asd asd asd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2292787440" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2269889165" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
